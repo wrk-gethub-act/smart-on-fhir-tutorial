fa0e8b6c8af504913769ecea211b1a12e6f1761f
wrk-gethub-act <gjaimez@ochca.com>
2024-09-18 11:40:15 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,9 +15,12 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: ['http://loinc.org|8302-2', //body height
+                             // 'http://loinc.org|8462-4', //diastolic blood pressure
+                             // 'http://loinc.org|8480-6', //systolic blood pressure
+                              'http://loinc.org|2085-9', //hdl
+                              'http://loinc.org|2089-1', //ldl
+                              'http://loinc.org|85354-9] //blood pressure systolic and diastolic
                       }
                     }
                   });
